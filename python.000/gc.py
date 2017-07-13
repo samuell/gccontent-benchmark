5a1df38733286122f1047da63362e4d8efd606eb
Samuel Lampa <samuel.lampa@gmail.com>
2017-07-13 13:44:33 +0200

    Add python implementation

---
+++ python.000/gc.py
@@ -0,0 +1,22 @@
+import re
+import string
+ 
+def main():
+    file = open("Homo_sapiens.GRCh37.67.dna_rm.chromosome.Y.fa","r")
+    a = 0
+    t = 0
+    g = 0
+    c = 0
+    for line in file:
+        if not line.startswith(">"):
+            g += line.count("G")
+            c += line.count("C")
+            a += line.count("A")
+            t += line.count("T")
+    totalBaseCount = a + t + c + g
+    gcCount = g + c
+    gcFraction = float(gcCount) / totalBaseCount
+    print( gcFraction * 100 )
+ 
+if __name__ == '__main__':
+    main()
