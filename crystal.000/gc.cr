b5e0dd6c65c77aad4a2c8949f1431e993c01dcb6
Samuel Lampa <samuel.lampa@rilnet.com>
2020-06-25 00:25:03 +0200

    Try to table optimize crystal code

--- crystal.000/gc.cr
+++ crystal.000/gc.cr
@@ -2,19 +2,28 @@ gcfile = File.new("Homo_sapiens.GRCh37.67.dna_rm.chromosome.Y.fa")
 
 at = 0
 gc = 0
+misc = 0
+
+cnts = Hash(Char, Pointer(Int32)).new
+
+cnts['A'] = pointerof(at)
+cnts['T'] = pointerof(at)
+cnts['G'] = pointerof(gc)
+cnts['C'] = pointerof(gc)
+cnts['N'] = pointerof(misc)
+cnts['\n'] = pointerof(misc)
 
 gcfile.each_line() do |line|
     if line.starts_with?('>')
         next
     end
     line.each_char() do |c|
-        if c == 'A' || c == 'T'
-            at += 1
-        elsif c == 'G' || c == 'C'
-            gc += 1
-        end
+        cnts[c].value += 1
     end
 end
+
+at = cnts['A'].value + cnts['T'].value
+gc = cnts['G'].value + cnts['C'].value
 
 gcfrac = gc / (gc + at) 
 
