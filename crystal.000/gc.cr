fe9c71dd9023fa5ee4ebc17aceb94f500a5d3978
Samuel Lampa <samuel.lampa@rilnet.com>
2020-06-25 17:45:43 +0200

    Compare with chars directly

--- crystal.000/gc.cr
+++ crystal.000/gc.cr
@@ -3,21 +3,16 @@ gcfile = File.new("Homo_sapiens.GRCh37.67.dna_rm.chromosome.Y.fa")
 at = 0
 gc = 0
 
-a = 'A'.ord()
-t = 'T'.ord()
-g = 'G'.ord()
-c = 'C'.ord()
-
 gcfile.each_line() do |line|
   if line.starts_with?('>')
     next
   end
   line.each_byte() do |chr|
     case chr
-    when a, t
+    when 'A', 'T'
       at += 1
       next
-    when g, c
+    when 'G', 'C'
       gc += 1
       next
     end
