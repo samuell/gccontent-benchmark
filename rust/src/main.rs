9a3461ca71fd97cc7cbf28745eb7625f26efd597
Rob Patro <rob-p@users.noreply.github.com>
2021-02-18 11:41:08 -0500

    more rust optimizations

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,32 +1,25 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufReader};
 use bstr::io::BufReadExt;
 
 fn main() {
     let filename = "Homo_sapiens.GRCh37.67.dna_rm.chromosome.Y.fa";
-
+    
+    let mut cc: [u32; 256] = [0; 256];
     // Open the file in read-only mode (ignoring errors).
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
-    let mut at = 0;
-    let mut gc = 0;
     // Read the file line by line using the lines() iterator from std::io::BufRead.
     reader.for_byte_line( |line|  {
         if line.first() == Some(&b'>') {
             return Ok(true)
         }
-        for c in line {
-            match c {
-                b'G' => gc += 1,
-                b'C' => gc += 1,
-                b'A' => at += 1,
-                b'T' => at += 1,
-                _ => (),
-            }
-        }
+        for c in line { cc[*c as usize] += 1; }
 	Ok(true)
-    });
+    }).unwrap();
+    let gc = cc[b'G' as usize] + cc[b'C' as usize];
+    let at = cc[b'A' as usize] + cc[b'T' as usize];
     let gc_ratio: f64 = gc as f64 / (gc as f64 + at as f64);
     println!("GC ratio (gc/(gc+at)): {}", gc_ratio);
 }
