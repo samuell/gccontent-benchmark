39213fe69bc28e7bcbbc32538da190bbf14a1e6d
Seth Stadick <sstadick@gmail.com>
2021-02-19 10:20:21 -0700

    Faster vectorized loop

--- rust.002.bitshift/src/main.rs
+++ rust.002.bitshift/src/main.rs
@@ -4,25 +4,15 @@ use std::io::{BufRead, BufReader};
 
 use std::intrinsics::unlikely;
 
-const VALUE: [u64; 256] = {
-    let mut array = [0u64; 256];
-    array[b'A' as usize] = 1;
-    array[b'T' as usize] = 1;
-    array[b'G' as usize] = 1 << 32;
-    array[b'C' as usize] = 1 << 32;
-    array
-};
-
-const CHUNKSIZE: usize = 16;
-
 fn main() {
     let filename = "chry_multiplied.fa";
 
     let file = File::open(filename).unwrap();
     let mut reader = BufReader::new(file);
 
-    let mut totals = 0u64;
     let mut line = Vec::with_capacity(100);
+    let mut at = 0;
+    let mut gc = 0;
     loop {
         if unlikely(reader.read_until(b'\n', &mut line).expect("error reading") == 0) {
             break;
@@ -32,21 +22,17 @@ fn main() {
             line.clear();
             continue;
         }
-        let mut chunker = line.chunks_exact(CHUNKSIZE);
-        while let Some(chars) = chunker.next() {
-            totals += (0..CHUNKSIZE)
-                .into_iter()
-                .map(|c| chars[c])
-                .map(|b| VALUE[b as usize])
-                .sum::<u64>();
-        }
-        for c in chunker.remainder() {
-            totals += VALUE[*c as usize];
+
+        // G/C & 10 == 2
+        // A/T & 10 == 0
+        // N & 10 == 10
+        for c in line.iter() {
+            gc += (*c & 10 == 2) as u32;
+            at += (*c & 10 == 0) as u32;
         }
         line.clear();
     }
-    let at = totals & 0xFFFFFFFF;
-    let gc = totals >> 32;
+
     let gc_ratio: f64 = gc as f64 / (gc as f64 + at as f64);
     println!("GC ratio (gc/(gc+at)): {}", gc_ratio);
 }
