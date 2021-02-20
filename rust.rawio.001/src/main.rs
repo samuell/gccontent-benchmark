3b7252cd1229d4ff3d16c23992f0ce8693639aad
Seth Stadick <sstadick@gmail.com>
2021-02-20 16:19:20 -0700

    vectorized io, no faster

--- rust.rawio.001/src/main.rs
+++ rust.rawio.001/src/main.rs
@@ -1,9 +1,10 @@
 #![feature(slice_internals)]
-use std::fs::File;
 use std::io::{BufReader, Read};
+use std::{fs::File, io::IoSliceMut};
 
 use core::slice::memchr;
 
+const BUFSIZE: usize = 32768;
 fn main() {
     let filename = "chry_multiplied.fa";
 
@@ -13,42 +14,61 @@ fn main() {
     let mut at = 0;
     let mut gc = 0;
 
-    let mut buffer = [0; 32768];
+    let mut buf1 = [0u8; BUFSIZE];
+    let mut buf2 = [0u8; BUFSIZE];
+    let mut buf3 = [0u8; BUFSIZE];
+    let mut buf4 = [0u8; BUFSIZE];
+    let mut buf5 = [0u8; BUFSIZE];
+    let mut buf6 = [0u8; BUFSIZE];
+    let mut buf7 = [0u8; BUFSIZE];
+    let mut buf8 = [0u8; BUFSIZE];
+    let mut bufs = [
+        IoSliceMut::new(&mut buf1),
+        IoSliceMut::new(&mut buf2),
+        IoSliceMut::new(&mut buf3),
+        IoSliceMut::new(&mut buf4),
+        IoSliceMut::new(&mut buf5),
+        IoSliceMut::new(&mut buf6),
+        IoSliceMut::new(&mut buf7),
+        IoSliceMut::new(&mut buf8),
+    ];
     let mut inheader = true;
 
     'reader: loop {
-        if reader.read(&mut buffer).expect("Read error") == 0 {
+        if reader.read_vectored(&mut bufs).expect("Read error") == 0 {
             break;
         }
 
-        let mut stop = 0;
-        loop {
-            let offset = if inheader {
-                match memchr::memchr(b'\n', &buffer[stop..buffer.len()]) {
-                    Some(pos) => {
-                        inheader = false;
-                        stop + pos + 1
+        for buffer in bufs.iter() {
+            let mut stop = 0;
+            loop {
+                let offset = if inheader {
+                    match memchr::memchr(b'\n', &buffer[stop..buffer.len()]) {
+                        Some(pos) => {
+                            inheader = false;
+                            stop + pos + 1
+                        }
+                        None => continue 'reader,
                     }
-                    None => continue 'reader,
+                } else {
+                    0
+                };
+
+                stop = match memchr::memchr(b'>', &buffer[offset..buffer.len()]) {
+                    Some(pos) => offset + pos,
+                    None => buffer.len(),
+                };
+                // Count records
+                for c in buffer[offset..stop].iter() {
+                    gc += (*c & 10 == 2) as u32;
+                    at += (*c & 10 == 0) as u32;
                 }
-            } else {
-                0
-            };
 
-            stop = match memchr::memchr(b'>', &buffer[offset..buffer.len()]) {
-                Some(pos) => offset + pos,
-                None => buffer.len(),
-            };
-            // Count records
-            for c in buffer[offset..stop].iter() {
-                gc += (*c & 10 == 2) as u32;
-                at += (*c & 10 == 0) as u32;
+                if stop == buffer.len() {
+                    break;
+                }
+                inheader = true;
             }
-
-            if stop == buffer.len() {
-                break;
-            }
-            inheader = true;
         }
     }
 
